40720df619ca070063c61c6120ea114ca78a160e
Antti Manninen <antti.e.manninen@tuni.fi>
2019-02-25 10:53:04 +0200

    first commit

---
+++ src/main/java/com/projectwork/coordinationgame/CoordinationgameApplication.java
@@ -0,0 +1,13 @@
+package com.projectwork.coordinationgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinationgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinationgameApplication.class, args);
+	}
+
+}
