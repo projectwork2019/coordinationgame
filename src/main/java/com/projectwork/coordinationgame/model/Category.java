140c5cf9dd14aa767ae1fba288b43f0f41830de1
Antti Manninen <antti.e.manninen@tuni.fi>
2019-03-03 15:44:52 +0200

    Added model classes for Category, Gaem and Presentation

---
+++ src/main/java/com/projectwork/coordinationgame/model/Category.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projectwork.coordinationgame.model;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+/**
+ *
+ * @author antti
+ */
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(generator = "category_generator")
+    @SequenceGenerator(
+            name = "category_generator",
+            sequenceName = "category_sequence",
+            initialValue = 1
+    )
+    private Long id;
+    
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+    
+    // Database relation mappings
+    @ManyToMany(mappedBy = "game")
+    private Set<Game> games;
+    
+    // Constructor (used only for initializing Set of games
+    public Category() {
+        this.games = new HashSet<Game>();
+    }
+    
+    // Getters and setters auto-generated
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    // equals and hashCode auto-generated
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        hash = 61 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Category other = (Category) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
---
+++ src/main/java/com/projectwork/coordinationgame/model/Game.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projectwork.coordinationgame.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import javax.json.JsonObject;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author antti
+ */
+@Entity 
+@Table(name = "game")
+public class Game {
+    @Id
+    @GeneratedValue(generator = "game_generator")
+    @SequenceGenerator(
+            name = "game_generator",
+            sequenceName = "game_sequence",
+            initialValue = 1
+    )
+    private Long id;
+    
+    @NotBlank
+    @Column(name = "gameData")
+    private JsonObject gameDataObject;  // Antti: Might not be compatible with JPA types, should be String (?)
+    
+    // Database relation mappings - Game belongs to multiple categories
+    @ManyToMany
+    @JoinTable(name = "game_category",
+            // Junction table key names to be updated to match the created database
+            // Foreign key referencing game-table primary key
+            joinColumns = {@JoinColumn(name = "fk_game")},
+            // Foreign key referecing category-table primary key
+            inverseJoinColumns = {@JoinColumn(name = "fk_category")}
+    )
+    private Set<Category> categories;
+    
+    // Database relation mappings - Game has multiple presentations
+    @OneToMany(mappedBy = "game")   // Refers to "game" field in Presentation class mapping
+    private List<Presentation> presentations;
+
+    // Constructor used only for initializing Set of categories and List of presentations
+    public Game() {
+        this.categories = new HashSet<Category>();
+        this.presentations = new ArrayList<Presentation>();
+    }
+
+    // getters and setters auto-generated
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public JsonObject getGameDataObject() {
+        return gameDataObject;
+    }
+
+    public void setGameDataObject(JsonObject gameDataObject) {
+        this.gameDataObject = gameDataObject;
+    }
+
+    // equals and hashCode auto-generated
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 11 * hash + Objects.hashCode(this.id);
+        hash = 11 * hash + Objects.hashCode(this.gameDataObject);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.gameDataObject, other.gameDataObject)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+}
