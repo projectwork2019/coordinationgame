09b3fc4202bd3ff2b8a7c1bdc16939f9befb978b
Antti Manninen <antti.e.manninen@tuni.fi>
2019-04-12 14:15:01 +0300

    Fixed error

--- src/main/java/com/projectwork/coordinationgame/model/GameSession.java
+++ src/main/java/com/projectwork/coordinationgame/model/GameSession.java
@@ -21,7 +21,7 @@ public class GameSession {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
-    private Set<Selection> selections;
+    
     private boolean firstTime;
     private boolean prevKnowledge;
     private LocalDateTime startTimestamp;
