b39edc2904e22bef9406a53432a34d16ec4cb647
Mohamad Hassan <mohamad.b.hassan@icloud.com>
2019-04-18 13:34:34 +0300

    Update GameSessionService.java

--- src/main/java/com/projectwork/coordinationgame/service/GameSessionService.java
+++ src/main/java/com/projectwork/coordinationgame/service/GameSessionService.java
@@ -59,7 +59,7 @@ public class GameSessionService {
         gameSessionDAO.closeCurrentSessionWithTransaction();
     }
  
-    public GameSessionDAO bookDao() {
+    public GameSessionDAO gameSessionDao() {
         return gameSessionDAO;
     }
 }
